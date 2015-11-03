2ce0bedd8852747bb78d3db0e26f1b2762b51e8a
alu0100818369 <alu0100818369@ull.edu.es>
2015-11-03 17:19:23 +0000

    Prueba de uno o varios autores

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -2,5 +2,12 @@ require 'spec_helper'
 require 'bibliografia/bibliog'
 
 describe Bibliog do
-    #it 'requires additional testing'
+    before :all do
+        @b1 = Bibliog.new(['Dave Thomas','Andy Hunt',' Chad Fowler'])
+    end
+    describe "# almacenamiento de autores" do
+        it "Debe existir uno o mas autores" do
+             @b1.autores.should eq(['Dave Thomas', 'Andy Hunt',' Chad Fowler'])
+        end
+    end
 end
