150d6ad2780679f04277e0d45237c772858a6eb8
alu0100816761 <alu0100816761@ull.edu.es>
2015-11-03 18:18:45 +0000

    Añadido el codigo para la fecha, y actualizado el initialize

--- lib/bibliografia/bibliog.rb
+++ lib/bibliografia/bibliog.rb
@@ -1,12 +1,14 @@
 class Bibliog
-    attr_reader :autores, :titulo, :serie, :editorial, :edicion
+    attr_reader :autores, :titulo, :serie, :editorial, :edicion, :mes, :anno
     
-    def initialize(a, t, e, ed, s="none")
+    def initialize(a, t, e, ed, mes, anno, s="none")
         @autores = a
         @titulo = t
         @serie = s
         @editorial = e
         @edicion = ed
+        @mes = mes
+        @anno = anno
     end
     
 end
