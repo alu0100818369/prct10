169dd10452ebdb770b299531468f14879a8704a3
alu0100816761 <alu0100816761@ull.edu.es>
2015-11-03 18:11:03 +0000

    Añadido el codigo para la edicion, actualizado initialize

--- lib/bibliografia/bibliog.rb
+++ lib/bibliografia/bibliog.rb
@@ -1,11 +1,12 @@
 class Bibliog
-    attr_reader :autores, :titulo, :serie, :editorial
+    attr_reader :autores, :titulo, :serie, :editorial, :edicion
     
-    def initialize(a, t, e, s="none")
+    def initialize(a, t, e, ed, s="none")
         @autores = a
         @titulo = t
         @serie = s
         @editorial = e
+        @edicion = ed
     end
     
 end
