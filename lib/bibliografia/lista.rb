d64ba4242b6afe3909f06816c164b9bea8413ac9
alu0100818369 <alu0100818369@ull.edu.es>
2015-11-10 15:22:35 +0000

    actual es nil al crear la lista

--- lib/bibliografia/lista.rb
+++ lib/bibliografia/lista.rb
@@ -1,8 +1,9 @@
 Nodo = Struct.new(:value, :next)
 
 class Lista
-    attr_accessor :cabeza
+    attr_accessor :cabeza, :actual
     def initialize
         @cabeza = nil
+        @actual = nil
     end
 end
