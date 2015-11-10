13c0fe99cc9307cbc5272b6af593329d40e6bb85
alu0100818369 <alu0100818369@ull.edu.es>
2015-11-10 16:43:07 +0000

    prueba con las bibliografias

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -109,9 +109,15 @@ describe Lista do
     before :all do
         @l = Lista.new
         @l1 = Lista.new
+        @l2 = Lista.new
         @aux = 5
         @aux1 = 7
         @aux2 = 9
+        @b1 = Bibliog.new(['Dave Thomas','Andy Hunt','Chad Fowler'], 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', 'Pragmatic Bookshelf', 4, 'July 7', 2013, ['9781937785499', '1937785491'], 'The Facets of Ruby')
+        @b2 = Bibliog.new('Scott Chacon','Pro Git 2009th Edition','Apress', 2009, 'August 27', 2009, ['9781430218333', '1430218339'], 'Pro')
+        @b3 = Bibliog.new(['David Flanagan', 'Yukihiro Matsumoto'], 'The Ruby Programming Language', 'O’Reilly Media', 1, 'February 4', 2008, ['0596516177', '9780596516178'])
+        @b4 = Bibliog.new(['David Chelimsky', 'Dave Astels', 'Bryan Helmkamp', 'Dan North', 'Zach Dennis', 'Aslak Hellesoy'], 'The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends', 'Pragmatic Bookshelf', 1, 'December 25', 2010, ['1934356379', '9781934356371'], 'The Facets of Ruby')
+        @b5 = Bibliog.new('Richard E', 'Silverman Git Pocket Guide', 'O’Reilly Media', 1, 'August 2', 2013, ['1449325866', '9781449325862'])
     end
     describe "Pruebas de la lista" do
         it "En la lista vacia la cabeza debe ser nil" do
@@ -133,6 +139,18 @@ describe Lista do
         it "Extraer primer elemento" do
             @l.insertar(@aux2)
             @l.pop.should eq(7)
+        end
+        it "mostrar bibliografia" do
+            @l2.insertar(@b1)
+            @l2.insertar(@b2)
+            @l2.insertar(@b3)
+            @l2.insertar(@b4)
+            @l2.insertar(@b5)
+            @l2.pop.should eq("Dave Thomas, Andy Hunt, Chad Fowler.\nProgramming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide\n(The Facets of Ruby)\nPragmatic Bookshelf; 4 edition (July 7, 2013)\nISBN-13: 978-1937785499\nISBN-10: 1937785491")
+            @l2.pop.should eq("Scott Chacon.\nPro Git 2009th Edition\n(Pro)\nApress; 2009 edition (August 27, 2009)\nISBN-13: 978-1430218333\nISBN-10: 1430218339")
+            @l2.pop.should eq("David Flanagan, Yukihiro Matsumoto.\nThe Ruby Programming Language\nO’Reilly Media; 1 edition (February 4, 2008)\nISBN-10: 0596516177\nISBN-13: 978-0596516178")
+            @l2.pop.should eq("David Chelimsky, Dave Astels, Bryan Helmkamp, Dan North, Zach Dennis, Aslak Hellesoy.\nThe RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends\n(The Facets of Ruby)\nPragmatic Bookshelf; 1 edition (December 25, 2010)\nISBN-10: 1934356379\n ISBN-13: 978-1934356371")
+            @l2.pop.should eq(" Richard E.\nSilverman Git Pocket Guide\nO’Reilly Media; 1 edition (August 2, 2013)\nISBN-10: 1449325866\nISBN-13: 978-1449325862")
         end
     end
 end
